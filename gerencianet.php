a992abd17ef47ca5b2b252cc612231542cd7189b
Pedro Augusto Moraes Nunes <89486643+PedroNunes7@users.noreply.github.com>
2023-07-27 18:02:43 -0300

    Start Project

---
+++ gerencianet.php
@@ -0,0 +1,70 @@
+<?php
+
+class Gerencianet {
+
+    private $urlHomologacao = 'https://api-pix-h.gerencianet.com.br';
+    private $urlProducao = 'https://api-pix.gerencianet.com.br';
+
+    private $client_Id = 'Client_Id_13b7837a2863d760ebaee942979565de768e8c69';
+    private $client_secrets = 'Client_Secret_e954e51aa5500af470362ed278a4898011d44843';
+    private $certificadoDigital = 'homologacao.pem';
+    
+    private $ambiente = 0;
+
+    function __construct($ambiente = 0) {
+        $this->ambiente = $ambiente;
+    }
+    
+    public function Autenticacao () {
+        $url = ($this->ambiente == 0 ? $this->urlProducao : $this->urlHomologacao) . '/oauth/token';
+
+        $auth = base64_encode("$this->client_Id:$this->client_secrets");
+
+        $header = array(
+            "Content-Type: application/json",
+            "Authorization: Basic $auth",
+        );
+
+        $response = $this->requisicao($url, [ "grant_type" => "client_credentials" ], $header, 'POST');
+        return $response;
+
+    }
+
+    private function requisicao($url, $data = array(), $header = array(), $type = "POST"){        
+        if($type == "GET" && is_array($data) && count($data) > 0)
+            $url = $url . "?" . http_build_query($data);
+        
+        $curl = curl_init($url);
+        
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_CUSTOMREQUEST, $type);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $header);
+        @curl_setopt($curl, CURLOPT_SAFE_UPLOAD, false);
+        curl_setopt($curl, CURLOPT_FOLLOWLOCATION, TRUE);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
+
+        // curl_setopt($curl, CURLOPT_SSLCERTTYPE, 'PEM');
+        curl_setopt($curl, CURLOPT_SSLCERT, realpath($this->certificadoDigital));
+        // curl_setopt($curl, CURLOPT_SSLCERTPASSWD, '');
+        
+        if($type == 'POST') {
+            curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($data));
+        }
+        
+        $curl_response = curl_exec($curl);
+
+        $response = curl_getinfo($curl);
+        $response['body'] = json_decode($curl_response, true);
+        $response['error'] = curl_error($curl);
+        curl_close($curl);
+
+        return $response;
+    }
+
+}
+
+
+$teste = new Gerencianet(1);
+
+var_dump($teste->Autenticacao());
